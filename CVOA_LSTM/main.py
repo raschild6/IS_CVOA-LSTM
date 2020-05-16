15d628f36865fbc13e644d8a1c30e2a5210573df
Luca <luca.perali@studenti.unipd.it>
2020-05-16 16:56:43 +0200

    change tensorflow log level, avoid using GPU

--- CVOA_LSTM/main.py
+++ CVOA_LSTM/main.py
@@ -2,10 +2,9 @@ from CVOA.CVOA import CVOA
 from ETL.ETL import *
 from DEEP_LEARNING.LSTM import *
 import time as time
-import os.path
+import os
 
 if __name__ == '__main__':
-
     my_path = os.path.abspath(os.path.dirname(__file__))
     path = os.path.join(my_path, "spain_energy_market_very_small.csv")
     # Load the dataset
@@ -22,8 +21,8 @@ if __name__ == '__main__':
     batch = 512         #1024 (changed also in LSTM, CVOA)
     
     # Initialize problem
-    cvoa = CVOA(size_fixed_part = 3, min_size_var_part = 2, max_size_var_part = 11, fixed_part_max_values = [5, 8], var_part_max_value = 11, max_time = 20,
-                xtrain = xtrain, ytrain=ytrain, xval=xval, yval=yval, pred_horizon=24, epochs=epochs, batch=batch, scaler = scaler)
+    cvoa = CVOA(size_fixed_part=3, min_size_var_part=2, max_size_var_part=11, fixed_part_max_values=[5, 8], var_part_max_value=11, max_time=20,
+                xtrain=xtrain, ytrain=ytrain, xval=xval, yval=yval, pred_horizon=24, epochs=epochs, batch=batch, scaler=scaler)
     time = int(round(time.time() * 1000))
     solution = cvoa.run()
     time = int(round(time.time() * 1000)) - time
