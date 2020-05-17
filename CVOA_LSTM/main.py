b41f71f010895cd2c0e3c616e140bf4334d60655
Michele <acikoagnag@gmail.com>
2020-05-17 09:47:51 +0100

    add very very small dataset

--- CVOA_LSTM/main.py
+++ CVOA_LSTM/main.py
@@ -7,7 +7,7 @@ import os.path
 if __name__ == '__main__':
 
     my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, "spain_energy_market_very_small.csv")
+    path = os.path.join(my_path, "spain_energy_market_very_very_small.csv")
     # Load the dataset
     data, scaler = load_data(path_to_data=path, useNormalization=True)
     # Transform data to a supervised dataset
