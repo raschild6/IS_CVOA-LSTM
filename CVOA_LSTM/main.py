8e14dd4e41583eabfcdc35c929d5320f292a334f
Luca <luca.perali@studenti.unipd.it>
2020-07-01 17:24:37 +0200

    add the possibility to test the model, add covid-francia.csv

--- CVOA_LSTM/main.py
+++ CVOA_LSTM/main.py
@@ -3,31 +3,39 @@ from ETL.ETL import *
 from DEEP_LEARNING.LSTM import *
 import time as t
 import os
+import sys
 
 if __name__ == '__main__':
     my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, "Covid-totali_positivi-andamento-nazionale.csv")
+    path = os.path.join(my_path, "covid-francia.csv")  # "Covid-totali_positivi-andamento-nazionale.csv")
     # Load the dataset
     data, scaler = load_data(path_to_data=path, useNormalization=True)
     # Transform data to a supervised dataset
     data = data_to_supervised(data, historical_window=9, prediction_horizon=1)
     # Split the dataset
-    xtrain, xtest, ytrain, ytest, xval, yval = splitData(data, historical_window=9, test_size=.01, val_size=.3)
+    xtrain, xtest, ytrain, ytest, xval, yval = splitData(data, historical_window=9, test_size=.9, val_size=.3)
     # Add shape to use LSTM network
     xtrain, xtest, ytrain, ytest, xval, yval = adaptShapesToLSTM(xtrain, xtest, ytrain, ytest, xval, yval)
 
     # Deep Learning parameters
     epochs = 10
     batch = 512         #1024 (changed also in LSTM, CVOA)
-    
-    # Initialize problem
-    cvoa = CVOA(size_fixed_part=3, min_size_var_part=2, max_size_var_part=11, fixed_part_max_values=[5, 8], var_part_max_value=11, max_time=20,
-                xtrain=xtrain, ytrain=ytrain, xval=xval, yval=yval, pred_horizon=1, epochs=epochs, batch=batch, scaler=scaler)
-    time = int(round(t.time() * 1000))
-    solution = cvoa.run()
-    time = int(round(t.time() * 1000)) - time
 
-    print("********************************************")
-    print("Best solution: " + str(solution))
-    print("Best fitness: ", "{:.4f}".format(solution.fitness))
-    print("Execution time: " + str((time) / 60000) + " mins")
+    if sys.argv[1] == "t":
+        model_path = sys.argv[2]
+        model = keras.models.load_model(model_path)
+        results = model.evaluate(xtest, ytest)
+        print(dict(zip(model.metrics_names, results)))
+    else:
+    
+        # Initialize problem
+        cvoa = CVOA(size_fixed_part=3, min_size_var_part=2, max_size_var_part=11, fixed_part_max_values=[5, 8], var_part_max_value=11, max_time=20,
+                    xtrain=xtrain, ytrain=ytrain, xval=xval, yval=yval, pred_horizon=1, epochs=epochs, batch=batch, scaler=scaler)
+        time = int(round(t.time() * 1000))
+        solution = cvoa.run()
+        time = int(round(t.time() * 1000)) - time
+
+        print("********************************************")
+        print("Best solution: " + str(solution))
+        print("Best fitness: ", "{:.4f}".format(solution.fitness))
+        print("Execution time: " + str((time) / 60000) + " mins")
