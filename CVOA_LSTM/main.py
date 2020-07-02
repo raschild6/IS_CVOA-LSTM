2597f59ca78ca4e93762ebfa309a845a50d85a7d
Luca <luca.perali@studenti.unipd.it>
2020-07-02 11:10:54 +0200

    remove the last 10 row of US.csv, add max_time = 10

--- CVOA_LSTM/main.py
+++ CVOA_LSTM/main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     # Transform data to a supervised dataset
     data = data_to_supervised(data, historical_window=9, prediction_horizon=1)
     # Split the dataset
-    xtrain, xtest, ytrain, ytest, xval, yval = splitData(data, historical_window=9, test_size=.3, val_size=.3)
+    xtrain, xtest, ytrain, ytest, xval, yval = splitData(data, historical_window=9, test_size=.1, val_size=.3)
     # Add shape to use LSTM network
     xtrain, xtest, ytrain, ytest, xval, yval = adaptShapesToLSTM(xtrain, xtest, ytrain, ytest, xval, yval)
 
@@ -22,8 +22,8 @@ if __name__ == '__main__':
     batch = 512         #1024 (changed also in LSTM, CVOA)
     
     # Initialize problem
-    cvoa = CVOA(size_fixed_part = 3, min_size_var_part = 2, max_size_var_part = 11, fixed_part_max_values = [5, 8], var_part_max_value = 11, max_time = 3,
-                xtrain = xtrain, ytrain=ytrain, xval=xval, yval=yval, pred_horizon=1, epochs=epochs, batch=batch, scaler = scaler)
+    cvoa = CVOA(size_fixed_part=3, min_size_var_part=2, max_size_var_part=11, fixed_part_max_values=[5, 8], var_part_max_value=11, max_time=10,
+                xtrain=xtrain, ytrain=ytrain, xval=xval, yval=yval, pred_horizon=1, epochs=epochs, batch=batch, scaler=scaler)
     time_init = int(round(t.time() * 1000))
     solution = cvoa.run()
     delta_time = int(round(t.time() * 1000)) - time_init
