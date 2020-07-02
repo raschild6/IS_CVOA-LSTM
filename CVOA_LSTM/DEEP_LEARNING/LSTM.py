472ecf7cf8dfee9a9183cb668f875d8551595aae
Luca <luca.perali@studenti.unipd.it>
2020-07-02 10:25:49 +0200

    commented physical_devices[0], IndexError: list index out of range

--- CVOA_LSTM/DEEP_LEARNING/LSTM.py
+++ CVOA_LSTM/DEEP_LEARNING/LSTM.py
@@ -21,7 +21,7 @@ config.gpu_options.allow_growth = True  # OOM error
 session = InteractiveSession(config=config)
 
 physical_devices = tf.config.list_physical_devices('GPU')  # Obtener la lista de GPU's instaladas en la maquina
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+#tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 print(tf.__version__)
 
